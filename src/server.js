166f5c5d50a49e640cfeec6712313ccbed95d07f
saleem-ux <saleem_diab86@yahoo.com>
2021-08-30 03:55:34 -0700

    lab08

---
+++ src/server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const notFoundHandler = require('./error-handlers/404.js');
+const errorHandler = require('./error-handlers/500.js');
+const logger = require('./middleware/logger.js');
+
+const authRoutes = require('./routes/routes.js');
+
+const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+
+// Routes
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
+app.use(authRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Working...');
+});
+
+app.get('/bad', (req, res, next) => {
+  next('Error Bad End Point');
+});
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error('Missing Port'); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
